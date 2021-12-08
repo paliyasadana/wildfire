9cbaf9f1a0f6caa360f40dd121044cb86646ef95
paliyasadana <92992612+paliyasadana@users.noreply.github.com>
2021-12-08 09:05:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ K.set_session(session)
 
 
 st.sidebar.markdown('Please upload a raw satellite image')
-uploaded_file = st.sidebar.file_uploader("Upload png file", type=["png"])
+uploaded_file = st.sidebar.file_uploader("Upload file")
 
 
 if uploaded_file is not None:
